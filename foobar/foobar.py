ceabe31d1b4b0e29a88e8fd63881645ae150c38d
whwang <whwang@isciencehub.com>
2018-08-29 14:49:54 +0800

    add source and tests

--- foobar/foobar.py
+++ foobar/foobar.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+
+def multiply(a, b):
+    """return a * b
+    """
+    return a * b
+
+
+class Dog(object):
+    """class for Dog"""
+    kind = 'canine'
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []
+
+    def add_trick(self, trick):
+        """add a trick"""
+        self.tricks.append(trick)
--- tests/test_foobar.py
+++ tests/test_foobar.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """Tests for `foobar` package."""
-
 import pytest
+from foobar.foobar import multiply, Dog
 
 
-from foobar import foobar
+def test_multiply_3_3():
+    assert multiply(3, 3) == 9
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_multiply_x_3():
+    assert multiply('x', 3) == 'xxx'
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_multiply_list_3():
+    res = multiply(['a', 'b'], 3)
+    assert res == ['a', 'b', 'a', 'b', 'a', 'b']
+
+
+@pytest.fixture()
+def twodog():
+    fido = Dog('Fido')
+    buddy = Dog('Buddy')
+    return fido, buddy
+
+
+class TestDog(object):
+    """test case of function multiply"""
+
+    def test_name(self, twodog):
+        fido, buddy = twodog
+        assert fido.name == 'Fido'
+        assert fido.tricks == []
+
+    def test_add_trick(self, twodog):
+        fido, buddy = twodog
+        fido.add_trick('roll over')
+        assert 'roll over' in fido.tricks
+
+    def test_add_trick_02(self, twodog):
+        fido, buddy = twodog
+        fido.add_trick('play dead')
+        buddy.add_trick('play dead')
+        assert 'roll over' in fido.tricks
+        assert 'play dead' in fido.tricks
+        assert 'play dead' in buddy.tricks
